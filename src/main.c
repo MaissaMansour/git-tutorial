5d9a11108f058b991fa434fef37ccfc904be1443
Alex Hoffman <alxhoff@gmail.com>
2022-10-26 19:10:09 +0200

    updaing list

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-int main() {
-   printf("Hello ESPL");
-   printf(" second challenge is coming !");
-   return 0;
+#include<espl_lib.h>
+int main() 
+{
+unsigned int n ;
+int condition ; 
+
+do{
+	printf("please enter a positive number\n " );
+	Scanf("%u",&n);
+	printf("%s\n",num_to_words(n));
+	printf(" Do you want to exit or run another number ? type 0 for exit and 1 for run\n ");
+	scanf("%d",condition); 
+} while (condition);
+
+return 0 ; 
 }
