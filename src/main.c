61092a3155c6bebdd673661541a450dff99abf11
Alex Hoffman <alxhoff@gmail.com>
2022-10-26 19:32:08 +0200

    second challenge done !

--- src/main.c
+++ src/main.c
@@ -3,15 +3,15 @@
 int main() 
 {
 unsigned int n ;
-int condition ; 
+int condition; 
 
 do{
 	printf("please enter a positive number\n " );
-	Scanf("%u",&n);
+	scanf("%u",&n);
 	printf("%s\n",num_to_words(n));
-	printf(" Do you want to exit or run another number ? type 0 for exit and 1 for run\n ");
-	scanf("%d",condition); 
+	printf(" Do you want to exit or run another number ? type 0 for exit and 1 for run. \n ");
+	scanf("%d",&condition); 
 } while (condition);
 
-return 0 ; 
+ 
 }
